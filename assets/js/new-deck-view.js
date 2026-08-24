9d378f82f9d8881042a1cce62d85b1e615808cf1
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-08-24 18:39:33 -0400

    Remove submit button 'disabled' class from function

--- assets/js/new-deck-view.js
+++ assets/js/new-deck-view.js
@@ -1,9 +1,12 @@
+// import { seeNewDeckView } from "./index.js";
+
 const formElement = document.querySelector(".new-deck-view__form");
 const textArea = formElement.querySelector(".new-deck-view__form-input");
 const submitButton = formElement.querySelector(".new-deck-view__submit-btn");
 
 function disableSubmitBtn() {
   submitButton.disabled = false;
+  mainElement.classList.remove("new-deck-view__submit-btn:disabled");
 }
 
 export { disableSubmitBtn };
