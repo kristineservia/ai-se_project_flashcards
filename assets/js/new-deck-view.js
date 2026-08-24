93dbbb97e23e1c3e64eed087232d9e212c7c5ecc
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-08-24 18:24:18 -0400

    Add function to enable submit button on New Deck form

--- assets/js/new-deck-view.js
+++ assets/js/new-deck-view.js
@@ -1,3 +1,7 @@
 const formElement = document.querySelector(".new-deck-view__form");
 const textArea = formElement.querySelector(".new-deck-view__form-input");
 const submitButton = formElement.querySelector(".new-deck-view__submit-btn");
+
+function disableSubmitBtn() {
+  submitButton.disabled = false;
+}
