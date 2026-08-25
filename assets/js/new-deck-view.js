073dea7010a816d135286c5de41019473dd26115
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-08-25 11:55:33 -0400

    Remove classList.remove() method from the disableSubmitBtn function

--- assets/js/new-deck-view.js
+++ assets/js/new-deck-view.js
@@ -1,12 +1,9 @@
-// import { seeNewDeckView } from "./index.js";
-
 const formElement = document.querySelector(".new-deck-view__form");
 const textArea = formElement.querySelector(".new-deck-view__form-input");
 const submitButton = formElement.querySelector(".new-deck-view__submit-btn");
 
 function disableSubmitBtn() {
   submitButton.disabled = false;
-  mainElement.classList.remove("new-deck-view__submit-btn:disabled");
 }
 
 export { disableSubmitBtn };
