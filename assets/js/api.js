6264148442a3696c6b1fedd9b25a7cd59b42964d
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-09-15 18:59:44 -0400

    Add processResponse() helper function to api.js to verify input

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,8 @@
 const baseURL = "https://se-flashcards-api.en.tripleten-services.com/v1";
+
+function processResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject(`Error: ${response.status}`);
+}
