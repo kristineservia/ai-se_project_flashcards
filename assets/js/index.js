380f77d48c026dd7efc6708788fc63a15219bdc7
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-09-17 14:36:42 -0400

    Import fetchedDecks array from decks.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-import { decks, getDeckByID } from "./decks.js";
+import { decks, getDeckByID, fetchedDecks } from "./decks.js";
 import { hexToString } from "./colors.js";
 import { renderHomeView } from "./home-view.js";
 import { renderDeckView } from "./deck-view.js";
