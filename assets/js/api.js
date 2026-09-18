bffe5cc30248c65f81a42754b5a7e6ec27d8dcca
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-09-18 18:18:55 -0400

    Create deleteDeck function to delete a deck from the API

--- assets/js/api.js
+++ assets/js/api.js
@@ -16,4 +16,11 @@ function getDecks() {
   return fetch(`${baseUrl}/decks`, { headers }).then(processResponse);
 }
 
+function deleteDeck(deckId) {
+  return fetch(`${baseUrl}/decks/${deckId}`, {
+    method: "DELETE",
+    headers: headers,
+  }).then(processResponse);
+}
+
 export { getDecks };
