08e1ddec8502d83026e2de1d7e7239e22b6a22d2
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-09-21 16:14:07 -0400

    Remove decks from import on home-view.js

--- assets/js/home-view.js
+++ assets/js/home-view.js
@@ -1,4 +1,4 @@
-import { decks, fetchedDecks, getDeckByID } from "./decks.js";
+import { fetchedDecks, getDeckByID } from "./decks.js";
 import { hexToString } from "./colors.js";
 import { renderConfirmationModal } from "./confirmation-modal.js";
 import { deleteDeck } from "./api.js";
